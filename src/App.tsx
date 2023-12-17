7a1701337b25c71535dba860f5a67098bb4b902a
Jonathon Orsi <jonathon.orsi@gmail.com>
2023-12-17 11:51:31 -0500

    one page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import './App.css';
 
 function App() {
     return (
-        <>
+        <main>
             <h1>a bit distorted</h1>
-            <nav>
-                <a href="/jons-tuner">Jon's Tuner</a>
-            </nav>
-        </>
+            <p>
+                <em>a digital agency in Toronto</em>
+            </p>
+
+            <br />
+            <br />
+
+            <h2>Jon's Tuner</h2>
+            <strong>Privacy Policy</strong>
+            <p>
+                Jon's Tuner records audio from your microphone to analyze and
+                display musical note and frequency information. All recorded
+                audio never leaves your device and is not saved.
+            </p>
+        </main>
     );
 }
 
