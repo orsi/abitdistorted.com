e8a5bfc9eccfa29287e3068200ee4b5cdc458aae
Jonathon Orsi <jonathon.orsi@gmail.com>
2024-02-06 23:01:44 -0500

    wip: more experimenting with webgl2

--- src/app/experiment-2/page.tsx
+++ src/app/experiment-2/page.tsx
@@ -1,132 +1,78 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { clear, createCanvas, draw, resize } from '../../hooks/useWebGL';
 
 export default function Experiment2() {
-    const ref = useRef<HTMLCanvasElement>(null);
+  const [canvasEl, setCanvasEl] = useState<HTMLCanvasElement>();
 
-    const resizeCanvas = (
-        canvas: HTMLCanvasElement,
-        gl: WebGL2RenderingContext
-    ) => {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        gl.viewport(0, 0, canvas.width, canvas.height);
-    };
-
-    useEffect(() => {
-        const canvas = ref.current;
-        if (!canvas) {
-            return;
-        }
-
-        const gl = ref.current?.getContext('webgl2');
-        if (!gl) {
-            return;
-        }
-
-        resizeCanvas(canvas, gl);
-
-        function onWindowResize(
-            canvas: HTMLCanvasElement,
-            gl: WebGL2RenderingContext
-        ) {
-            resizeCanvas(canvas, gl);
-        }
-
-        window.addEventListener(
-            'resize',
-            onWindowResize.bind(null, canvas, gl)
-        );
-
-        const program = gl.createProgram();
-        if (!program) {
-            return;
-        }
-
-        const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-        if (!vertexShader) {
-            return;
-        }
-        gl.shaderSource(
-            vertexShader,
-            `#version 300 es
-        in vec2 position;
-        void main() {
-            gl_Position = vec4(position, 0.0, 1.0);
-            gl_PointSize = 10.0;
-        }`
-        );
-        gl.compileShader(vertexShader);
-        gl.attachShader(program, vertexShader);
-
-        const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-        if (!fragmentShader) {
-            return;
-        }
-        gl.shaderSource(
-            fragmentShader,
-            `#version 300 es
-        precision mediump float;
-        out vec4 outColor;
-        void main() {
-            outColor = vec4(1.0, 0.0, 0.0, 1.0);
-        }`
-        );
-        gl.compileShader(fragmentShader);
-        gl.attachShader(program, fragmentShader);
-
-        gl.linkProgram(program);
-        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-            const vertexShaderInfo = `${gl.getShaderInfoLog(vertexShader)}`;
-            const fragmentShaderInfo = `${gl.getShaderInfoLog(fragmentShader)}`;
-            const programInfo = `${gl.getProgramInfoLog(program)}`;
-            throw new Error(`
-            Vertex: ${vertexShaderInfo}
-            Fragment: ${fragmentShaderInfo}
-            Program: ${programInfo}
-            `);
-        }
-
-        const vertexArray = gl.createVertexArray();
-        gl.bindVertexArray(vertexArray);
-
-        const buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-        gl.bufferData(
-            gl.ARRAY_BUFFER,
-            new Float32Array([0.5, 0.5]),
-            gl.STATIC_DRAW
-        );
-
-        gl.useProgram(program);
-        gl.bindVertexArray(vertexArray);
-
-        const positionLocation = gl.getAttribLocation(program, 'position');
-        gl.enableVertexAttribArray(positionLocation);
-        gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-
-        gl.clearColor(0, 0, 0, 1);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-
-        gl.drawArrays(gl.POINTS, 0, 1);
-
-        return () => {
-            window.removeEventListener(
-                'resize',
-                onWindowResize.bind(null, canvas, gl)
-            );
-        };
+  useEffect(() => {
+    const { canvas, gl, program } = createCanvas('experiment-2', {
+      vertexShader: `#version 300 es
+              in vec2 position;
+              void main() {
+                  gl_Position = vec4(position, 0.0, 1.0);
+                  gl_PointSize = 10.0;
+              }`,
+      fragmentShader: `#version 300 es
+              precision mediump float;
+              out vec4 outColor;
+              void main() {
+                  outColor = vec4(1.0, 1.0, 0.0, 1.0);
+              }`,
     });
 
-    return (
-        <canvas
-            ref={ref}
-            style={{
-                position: 'fixed',
-                top: 0,
-                left: 0,
-            }}
-        />
+    setCanvasEl(canvas);
+
+    const vertexArray = gl.createVertexArray();
+    gl.bindVertexArray(vertexArray);
+
+    const buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array([0.5, 0.5]),
+      gl.STATIC_DRAW
     );
+
+    gl.bindVertexArray(vertexArray);
+
+    const positionLocation = gl.getAttribLocation(program, 'position');
+    gl.enableVertexAttribArray(positionLocation);
+    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+    clear(gl, { clearColor: { r: 1 } });
+    draw(gl, {
+      drawMode: 'points',
+      drawCount: 1,
+    });
+
+    // setup resize
+    resize(gl, window.innerWidth, window.innerHeight);
+
+    function onWindowResize(e: Event) {
+      resize(gl!, window.innerWidth, window.innerHeight);
+    }
+
+    window.addEventListener('resize', onWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', onWindowResize);
+    };
+  });
+
+  return (
+    <div
+      ref={(ref) => {
+        if (ref && canvasEl) {
+          ref.appendChild(canvasEl);
+        }
+      }}
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+      }}
+    ></div>
+  );
 }
--- src/hooks/useWebGL.tsx
+++ src/hooks/useWebGL.tsx
@@ -2,71 +2,171 @@
 
 import { useRef, useEffect } from 'react';
 
-export function useWebGL(
-    vertexShader: string,
-    fragmentShader: string,
-    update: (gl: WebGL2RenderingContext) => void
+interface WebGL2Options {
+  vertexShader: string;
+  fragmentShader: string;
+}
+
+const canvases: Record<string, HTMLCanvasElement> = {};
+
+export function createCanvas(
+  id: string,
+  { vertexShader, fragmentShader }: WebGL2Options
 ) {
-    const ref = useRef<HTMLCanvasElement>(null);
+  if (canvases[id]) {
+    console.warn(`A canvas with id ${id} already exists!`);
+  } else {
+    canvases[id] = document.createElement('canvas');
+  }
 
-    useEffect(() => {
-        if (ref.current == null) {
-            console.error('HTMLCanvasElement is null');
-            return;
-        }
-        if (vertexShader == null) {
-            console.error('Vertex Shader is null');
-            return;
-        }
-        if (fragmentShader == null) {
-            console.error('Fragment Shader is null');
-            return;
-        }
+  const gl = canvases[id].getContext('webgl2')!;
+  const program = gl.createProgram()!;
 
-        const gl = ref.current.getContext('webgl2')!;
-        const program = gl.createProgram()!;
+  // setup shaders
+  if (vertexShader) {
+    setupShader(gl, 'vertex', program, vertexShader);
+  }
 
-        // vertex shader
-        const glVertexShader = gl.createShader(gl.VERTEX_SHADER)!;
-        gl.shaderSource(glVertexShader, vertexShader);
-        gl.compileShader(glVertexShader);
-        gl.attachShader(program, glVertexShader);
+  if (fragmentShader) {
+    setupShader(gl, 'fragment', program, fragmentShader);
+  }
 
-        // fragment shader
-        const glFragmentShader = gl.createShader(gl.FRAGMENT_SHADER)!;
-        gl.shaderSource(glFragmentShader, fragmentShader);
-        gl.compileShader(glFragmentShader);
-        gl.attachShader(program, glFragmentShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error(gl.getProgramInfoLog(program));
+  }
+  gl.useProgram(program);
 
-        // program
-        gl.linkProgram(program);
-        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-            const info = gl.getProgramInfoLog(program);
-            throw `Could not compile WebGL program. \n\n${info}`;
-        }
+  return {
+    canvas: canvases[id],
+    gl,
+    program,
+  };
+}
 
-        gl.useProgram(program);
-        return () => {
-            console.log('deletE?');
-            gl.deleteProgram(program);
-        };
-    }, [vertexShader, fragmentShader]);
+export function setupShader(
+  gl: WebGL2RenderingContext,
+  type: 'vertex' | 'fragment',
+  program: WebGLProgram,
+  source: string
+) {
+  const glShader = gl.createShader(
+    type === 'vertex' ? gl.VERTEX_SHADER : gl.FRAGMENT_SHADER
+  )!;
+  gl.shaderSource(glShader, source);
+  gl.compileShader(glShader);
 
-    useEffect(() => {
-        let frame = 0;
-        function raf() {
-            const gl = ref.current?.getContext('webgl2')!;
-            update(gl);
-            frame = requestAnimationFrame(raf);
-        }
-        frame = requestAnimationFrame(raf);
+  if (!gl.getShaderParameter(glShader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(glShader));
+  }
 
-        return () => {
-            cancelAnimationFrame(frame);
-        };
-    }, [update]);
+  gl.attachShader(program, glShader);
+}
 
-    return (props: React.HTMLAttributes<HTMLCanvasElement>) => (
-        <canvas {...props} ref={ref} />
-    );
+interface ClearOptions {
+  clearColor?: { r?: number; g?: number; b?: number; a?: number };
+}
+export function clear(gl: WebGL2RenderingContext, options?: ClearOptions) {
+  gl.clearColor(
+    options?.clearColor?.r ?? 0,
+    options?.clearColor?.g ?? 0,
+    options?.clearColor?.b ?? 0,
+    options?.clearColor?.a ?? 1
+  );
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+interface DrawOptions {
+  drawCount?: number;
+  drawFirst?: number;
+  drawMode?: 'points' | 'lines' | 'triangles';
+}
+export function draw(
+  gl: WebGL2RenderingContext,
+  { drawCount, drawFirst, drawMode }: DrawOptions
+) {
+  let mode: number = gl.TRIANGLES;
+  if (drawMode === 'points') {
+    mode = gl.POINTS;
+  } else if (drawMode === 'lines') {
+    mode = gl.LINES;
+  }
+  gl.drawArrays(mode, drawFirst ?? 0, drawCount ?? 0);
+}
+
+export function resize(
+  gl: WebGL2RenderingContext,
+  width: number,
+  height: number
+) {
+  gl.canvas.width = width;
+  gl.canvas.height = height;
+  gl.viewport(0, 0, width, height);
+}
+
+export function useWebGL(
+  vertexShader: string,
+  fragmentShader: string,
+  update: (gl: WebGL2RenderingContext) => void
+) {
+  const ref = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (ref.current == null) {
+      console.error('HTMLCanvasElement is null');
+      return;
+    }
+    if (vertexShader == null) {
+      console.error('Vertex Shader is null');
+      return;
+    }
+    if (fragmentShader == null) {
+      console.error('Fragment Shader is null');
+      return;
+    }
+
+    const gl = ref.current.getContext('webgl2')!;
+    const program = gl.createProgram()!;
+
+    // vertex shader
+    const glVertexShader = gl.createShader(gl.VERTEX_SHADER)!;
+    gl.shaderSource(glVertexShader, vertexShader);
+    gl.compileShader(glVertexShader);
+    gl.attachShader(program, glVertexShader);
+
+    // fragment shader
+    const glFragmentShader = gl.createShader(gl.FRAGMENT_SHADER)!;
+    gl.shaderSource(glFragmentShader, fragmentShader);
+    gl.compileShader(glFragmentShader);
+    gl.attachShader(program, glFragmentShader);
+
+    // program
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      const info = gl.getProgramInfoLog(program);
+      throw `Could not compile WebGL program. \n\n${info}`;
+    }
+
+    gl.useProgram(program);
+    return () => {
+      console.log('deletE?');
+      gl.deleteProgram(program);
+    };
+  }, [vertexShader, fragmentShader]);
+
+  useEffect(() => {
+    let frame = 0;
+    function raf() {
+      const gl = ref.current?.getContext('webgl2')!;
+      update(gl);
+      frame = requestAnimationFrame(raf);
+    }
+    frame = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(frame);
+    };
+  }, [update]);
+
+  return <canvas ref={ref} />;
 }
